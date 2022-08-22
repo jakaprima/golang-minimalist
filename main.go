3c77623c72455adc2c423f2d166fe8d3cf6ccd12
jaka.maulana <=>
2022-08-22 20:23:49 +0700

    feat: slice

--- main.go
+++ main.go
@@ -3,12 +3,21 @@ package main
 import "fmt"
 
 func main() {
+	// ----------------------- SLICE
+	var students = []string{"Jaka", "Toni", "Hans", "Stevanus", "Edwin Nugroho", "Kevin Hugo", "Rizki Ramadhan", "Fajar Agus Maulana", "Iqbal Hamdani", "Fahmi Tajuddin"}
+	_ = students
+	for i, data := range students {
+		fmt.Println("orang ke: ", i+1)
+		fmt.Println("nama: ", data)
+	}
+
+	// ------------------------ LOOP, Condition
 	for i := 0; i < 10; i++ {
 		if i%2 == 0 {
 			fmt.Println(i, " genap")
 		} else {
 			fmt.Println(i, " ganjil")
 		}
-
 	}
+
 }
---
+++ slice.go
@@ -0,0 +1,14 @@
+// // TODO: jadiin satu dulu
+// package main
+
+// import "fmt"
+
+// func slice() {
+// 	// ----------------------- SLICE
+// 	var students = []string{"Jaka", "Toni", "Hans", "Stevanus", "Edwin Nugroho", "Kevin Hugo", "Rizki Ramadhan", "Fajar Agus Maulana", "Iqbal Hamdani", "Fahmi Tajuddin"}
+// 	_ = students
+// 	for i, data := range students {
+// 		fmt.Println("orang ke: ", i+1)
+// 		fmt.Println("nama: ", data)
+// 	}
+// }
